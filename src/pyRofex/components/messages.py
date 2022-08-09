6676c0703b6965a3121e9e54a8567a3ad5117c04
UrielPaluch <uripaluch2@gmail.com>
2022-08-09 16:50:30 -0300

    Arreglo corchetes en SEND_ORDER

--- src/pyRofex/components/messages.py
+++ src/pyRofex/components/messages.py
@@ -14,4 +14,4 @@ INSTRUMENT = '{{"symbol":"{ticker}","marketId":"{market}"}}'
 # Template to insert a Double Quote
 DOUBLE_QUOTES = '"{item}"'
 # Template for sending an Order via WebSocket
-SEND_ORDER = '{{"type":"no","product":{"marketId":"ROFX","symbol":"{ticker}"},"price":"{price}","quantity":"{size}","side":"{side}","account":"{account}"}}'
+SEND_ORDER = '{{"type":"no","product":{{"marketId":"ROFX","symbol":"{ticker}"}},"price":"{price}","quantity":"{size}","side":"{side}","account":"{account}"}}'
